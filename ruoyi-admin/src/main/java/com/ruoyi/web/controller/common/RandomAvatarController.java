d7c6149176e4cac271ac9d54fc6d96aa928e6993
fx67ll <2717745014@qq.com>
2023-07-25 10:41:13 +0800

    add image steam api

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/RandomAvatarController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/RandomAvatarController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import javax.imageio.ImageIO;
@@ -33,16 +34,19 @@ public class RandomAvatarController {
 
     /**
      * 图片宽
+     * 允许自定义，通过传参体现，这里为默认值
      */
-    private final static int IMG_WIDTH = 360;
+    private final static int IMG_WIDTH = 230;
 
     /**
      * 图片高
+     * 允许自定义，通过传参体现，这里为默认值
      */
-    private final static int IMG_HEIGHT = 360;
+    private final static int IMG_HEIGHT = 230;
 
     /**
      * 图片边缘内边距
+     * 允许自定义，通过传参体现，这里为默认值
      */
     private final static int PADDING = 30;
 
@@ -53,6 +57,7 @@ public class RandomAvatarController {
 
     /**
      * 每边矩形数量（建议>=5）
+     * 允许自定义，通过传参体现，这里为默认值
      */
     private final static int BLOCK_NUM = 9;
 
@@ -76,30 +81,58 @@ public class RandomAvatarController {
      * 直接返回随机马赛克图片流
      */
     @GetMapping("/getRandomAvatar")
-    public void getAvatar(HttpServletResponse response) throws Exception {
+    public void getAvatar(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        //设置输出文件格式
+        String avatarWidthValue = request.getParameter("avatarWidth");
+        String avatarHeightValue = request.getParameter("avatarHeight");
+        String avatarPaddingValue = request.getParameter("avatarPadding");
+        String avatarBlockNumValue = request.getParameter("avatarBlockNum");
+        String isNeedMoreMosaic = request.getParameter("isNeedMoreMosaic");
+
+        int avatarWidth = avatarWidthValue != null && !avatarWidthValue.isEmpty() ? Integer.parseInt(avatarWidthValue) : IMG_WIDTH;
+        int avatarHeight = avatarHeightValue != null && !avatarHeightValue.isEmpty() ? Integer.parseInt(avatarHeightValue) : IMG_HEIGHT;
+        int avatarPadding = avatarPaddingValue != null && !avatarPaddingValue.isEmpty() ? Integer.parseInt(avatarPaddingValue) : PADDING;
+        int avatarBlockNum = avatarBlockNumValue != null && !avatarBlockNumValue.isEmpty() ? Integer.parseInt(avatarBlockNumValue) : BLOCK_NUM;
+
+        // 设置输出文件格式
         response.setContentType("image/png");
 
         // 得到图片缓冲区
         BufferedImage bi = new BufferedImage
-                (IMG_WIDTH, IMG_HEIGHT, BufferedImage.TYPE_INT_RGB);
+                (avatarWidth, avatarHeight, BufferedImage.TYPE_INT_RGB);
+
         // 得到它的绘制环境(这张图片的笔)
         Graphics2D g2 = (Graphics2D) bi.getGraphics();
+
         // 设置背景颜色
         g2.setColor(BACK_GROUND_COLOR);
-        g2.fillRect(0, 0, IMG_WIDTH, IMG_HEIGHT);
+        g2.fillRect(0, 0, avatarWidth, avatarHeight);
+
         // 随机颜色
         Color mainColor = getRandomColor();
-        // 随机生成有效块坐标集合
-        double randomRadioNum = randomDecimalNumber();
-        System.out.println("randomRadioNum:" + randomRadioNum);
-        List<Point> pointList = getRandomPointList(randomRadioNum);
-        // 填充图形
-        fillGraph(g2, pointList, mainColor);
 
+        // 随机生成有效块坐标集合
+        double randomRadioNum = 0;
+        try {
+            randomRadioNum = randomDecimalNumber(isNeedMoreMosaic);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("randomRadioNum:" + randomRadioNum);
+        System.out.println("========================================");
+        List<Point> pointList = getRandomPointList(randomRadioNum, avatarBlockNum);
+
+        // 填充图形
+        fillGraph(g2, pointList, mainColor, avatarWidth, avatarHeight, avatarPadding, avatarBlockNum);
+
+        // 下面都是关于流处理的流程
         // 获取绘制好的图片的InputStream对象
-        InputStream in = getImageStream(bi);
+        InputStream in = null;
+        try {
+            in = getImageStream(bi);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
         // 创建存放文件内容的数组
         byte[] buff = new byte[1024];
@@ -121,32 +154,60 @@ public class RandomAvatarController {
      * 返回随机马赛克图片的Base64
      */
     @GetMapping("/getRandomAvatarByBase64")
-    public AjaxResult getAvatarBase64(HttpServletResponse response) throws Exception {
+    public AjaxResult getAvatarBase64(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        String avatarWidthValue = request.getParameter("avatarWidth");
+        String avatarHeightValue = request.getParameter("avatarHeight");
+        String avatarPaddingValue = request.getParameter("avatarPadding");
+        String avatarBlockNumValue = request.getParameter("avatarBlockNum");
+        String isNeedMoreMosaic = request.getParameter("isNeedMoreMosaic");
+
+        int avatarWidth = avatarWidthValue != null && !avatarWidthValue.isEmpty() ? Integer.parseInt(avatarWidthValue) : IMG_WIDTH;
+        int avatarHeight = avatarHeightValue != null && !avatarHeightValue.isEmpty() ? Integer.parseInt(avatarHeightValue) : IMG_HEIGHT;
+        int avatarPadding = avatarPaddingValue != null && !avatarPaddingValue.isEmpty() ? Integer.parseInt(avatarPaddingValue) : PADDING;
+        int avatarBlockNum = avatarBlockNumValue != null && !avatarBlockNumValue.isEmpty() ? Integer.parseInt(avatarBlockNumValue) : BLOCK_NUM;
+
+        if (avatarWidth < 0 && avatarHeight < 0 && avatarPadding < 0) {
+            AjaxResult ajax = AjaxResult.warn("非法图片参数！");
+            return ajax;
+        }
+
+        if (!isRightABN(avatarBlockNumValue)) {
+            AjaxResult ajax = AjaxResult.warn("每边矩形数量请大于4小于11！");
+            return ajax;
+        }
 
         //设置输出文件格式
         response.setContentType("image/png");
 
         // 得到图片缓冲区
         BufferedImage bi = new BufferedImage
-                (IMG_WIDTH, IMG_HEIGHT, BufferedImage.TYPE_INT_RGB);
+                (avatarWidth, avatarHeight, BufferedImage.TYPE_INT_RGB);
+
         // 得到它的绘制环境(这张图片的笔)
         Graphics2D g2 = (Graphics2D) bi.getGraphics();
+
         // 设置背景颜色
         g2.setColor(BACK_GROUND_COLOR);
-        g2.fillRect(0, 0, IMG_WIDTH, IMG_HEIGHT);
+        g2.fillRect(0, 0, avatarWidth, avatarHeight);
+
         // 随机颜色
         Color mainColor = getRandomColor();
+
         // 随机生成有效块坐标集合
-        double randomRadioNum = randomDecimalNumber();
+        double randomRadioNum = randomDecimalNumber(isNeedMoreMosaic);
         System.out.println("randomRadioNum:" + randomRadioNum);
-        List<Point> pointList = getRandomPointList(randomRadioNum);
+        System.out.println("========================================");
+        List<Point> pointList = getRandomPointList(randomRadioNum, avatarBlockNum);
+
         // 填充图形
-        fillGraph(g2, pointList, mainColor);
+        fillGraph(g2, pointList, mainColor, avatarWidth, avatarHeight, avatarPadding, avatarBlockNum);
 
         // 获取绘制好的图片的InputStream对象
         InputStream in = getImageStream(bi);
         String base = inputStream2Base64(in);
 
+        // 返回base64信息
         AjaxResult ajax = AjaxResult.success();
         ajax.put("avatar", base);
         return ajax;
@@ -157,31 +218,53 @@ public class RandomAvatarController {
      * 生成随机马赛克图片，保存后返回图片地址信息（暂时不做）
      */
     @GetMapping("/getRandomAvatarUrl")
-    public AjaxResult getAvatarUrl(HttpServletResponse response) throws Exception {
-//    public AjaxResult getAvatarUrl(HttpServletResponse response) throws IOException {
+    public AjaxResult getAvatarUrl(HttpServletRequest request) throws Exception {
+
+        String avatarWidthValue = request.getParameter("avatarWidth");
+        String avatarHeightValue = request.getParameter("avatarHeight");
+        String avatarPaddingValue = request.getParameter("avatarPadding");
+        String avatarBlockNumValue = request.getParameter("avatarBlockNum");
+        String isNeedMoreMosaic = request.getParameter("isNeedMoreMosaic");
+
+        int avatarWidth = avatarWidthValue != null && !avatarWidthValue.isEmpty() ? Integer.parseInt(avatarWidthValue) : IMG_WIDTH;
+        int avatarHeight = avatarHeightValue != null && !avatarHeightValue.isEmpty() ? Integer.parseInt(avatarHeightValue) : IMG_HEIGHT;
+        int avatarPadding = avatarPaddingValue != null && !avatarPaddingValue.isEmpty() ? Integer.parseInt(avatarPaddingValue) : PADDING;
+        int avatarBlockNum = avatarBlockNumValue != null && !avatarBlockNumValue.isEmpty() ? Integer.parseInt(avatarBlockNumValue) : BLOCK_NUM;
+
+        if (avatarWidth < 0 && avatarHeight < 0 && avatarPadding < 0) {
+            AjaxResult ajax = AjaxResult.warn("非法图片参数！");
+            return ajax;
+        }
+
+        if (!isRightABN(avatarBlockNumValue)) {
+            AjaxResult ajax = AjaxResult.warn("每边矩形数量请大于5小于10！");
+            return ajax;
+        }
 
         // 得到图片缓冲区
         BufferedImage bi = new BufferedImage
-                (IMG_WIDTH, IMG_HEIGHT, BufferedImage.TYPE_INT_RGB);
+                (avatarWidth, avatarHeight, BufferedImage.TYPE_INT_RGB);
 
         // 得到它的绘制环境(这张图片的笔)
         Graphics2D g2 = (Graphics2D) bi.getGraphics();
 
         // 设置背景颜色
         g2.setColor(BACK_GROUND_COLOR);
-        g2.fillRect(0, 0, IMG_WIDTH, IMG_HEIGHT);
+        g2.fillRect(0, 0, avatarWidth, avatarHeight);
 
         // 随机颜色
         Color mainColor = getRandomColor();
 
         // 随机生成有效块坐标集合
-        double randomRadioNum = randomDecimalNumber();
+        double randomRadioNum = randomDecimalNumber(isNeedMoreMosaic);
         System.out.println("randomRadioNum:" + randomRadioNum);
-        List<Point> pointList = getRandomPointList(randomRadioNum);
+        System.out.println("========================================");
+        List<Point> pointList = getRandomPointList(randomRadioNum, avatarBlockNum);
 
         // 填充图形
-        fillGraph(g2, pointList, mainColor);
+        fillGraph(g2, pointList, mainColor, avatarWidth, avatarHeight, avatarPadding, avatarBlockNum);
 
+        // 通过文件存储流
 //        File file = new File(DIR);
 //        if (!file.exists()) {
 //            file.mkdirs();
@@ -189,6 +272,7 @@ public class RandomAvatarController {
 //        ImageIO.write(bi,"JPG",
 //                new FileOutputStream(DIR+System.currentTimeMillis()+".jpg"));
 
+        // 测试返参
         AjaxResult ajax = AjaxResult.success();
         ajax.put("test", "fx67ll");
         return ajax;
@@ -200,7 +284,7 @@ public class RandomAvatarController {
      * @param bi 图片文件
      * @return is 文件流
      */
-    private static InputStream getImageStream(BufferedImage bi) {
+    private static InputStream getImageStream(BufferedImage bi) throws Exception {
         InputStream is = null;
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         ImageOutputStream imos;
@@ -209,7 +293,7 @@ public class RandomAvatarController {
             ImageIO.write(bi, "png", imos);
             is = new ByteArrayInputStream(baos.toByteArray());
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new Exception("图片转流失败");
         }
         return is;
     }
@@ -247,19 +331,44 @@ public class RandomAvatarController {
     }
 
     /**
-     * 生成一个随机两位小数，范围是0.25-0.75
+     * 生成一个随机两位小数，范围是0.2-0.85
      *
+     * @param isNeedMoreMosaic 需要更多马赛克还是更少
      * @return Double
      * @throws Exception
      */
-    private static Double randomDecimalNumber() throws Exception {
-        Random random = new Random();
-        double randomDecimal = random.nextDouble() * 0.5 + 0.25;
-        randomDecimal = Math.round(randomDecimal * 100) / 100.0;
-        return randomDecimal;
-//        return Math.round((new Random().nextDouble() * 0.75 + 0.25) * 100) / 100.0;
+    private static Double randomDecimalNumber(String isNeedMoreMosaic) throws Exception {
+        try {
+            Random random = new Random();
+            Double randomDecimal = random.nextDouble() * 0.65 + 0.2;
+            Double lowDecimal = new Double("0.4");
+            Double highDecimal = new Double("0.6");
+            randomDecimal = Math.round(randomDecimal * 100) / 100.0;
+            if (isNeedMoreMosaic.equals('Y') && randomDecimal.compareTo(highDecimal) < 0) {
+                return randomDecimalNumber(isNeedMoreMosaic);
+            } else if (isNeedMoreMosaic.equals('N') && randomDecimal.compareTo(lowDecimal) > 0) {
+                return randomDecimalNumber(isNeedMoreMosaic);
+            } else {
+                return randomDecimal;
+            }
+        } catch (Exception e) {
+            throw new Exception("获取随机数异常");
+        }
     }
 
+    /**
+     * 校验参数avatarBlockNum是否合法
+     *
+     * @param avatarBlockNum 马赛克矩形数量
+     * @return Boolean
+     * @throws Exception
+     */
+    private static Boolean isRightABN(String avatarBlockNum) {
+        if (Integer.parseInt(avatarBlockNum) > 4 && Integer.parseInt(avatarBlockNum) < 11) {
+            return true;
+        }
+        return false;
+    }
 
     /**
      * 填充图形
@@ -268,15 +377,17 @@ public class RandomAvatarController {
      * @param pointList 填充块坐标
      * @param mainColor 填充颜色
      */
-    private static void fillGraph(Graphics2D g2, List<Point> pointList, Color mainColor) {
-        int rowBlockLength = (IMG_HEIGHT - 2 * PADDING) / BLOCK_NUM;
-        int colBlockLength = (IMG_WIDTH - 2 * PADDING) / BLOCK_NUM;
+    private static void fillGraph(Graphics2D g2, List<Point> pointList, Color mainColor, Integer imgWidth, Integer imgHeight, Integer imgPadding, Integer blockNum) {
+        int rowBlockLength = (imgWidth - 2 * imgPadding) / blockNum;
+        int colBlockLength = (imgHeight - 2 * imgPadding) / blockNum;
+
         // 填充
         g2.setColor(mainColor);
+
         // 遍历points
         for (Point point : pointList) {
-            g2.fillRect(PADDING + point.x * rowBlockLength,
-                    PADDING + point.y * colBlockLength,
+            g2.fillRect(imgPadding + point.x * rowBlockLength,
+                    imgPadding + point.y * colBlockLength,
                     rowBlockLength, colBlockLength);
         }
     }
@@ -287,20 +398,20 @@ public class RandomAvatarController {
      * @param radio 填充色块几率
      * @return List<Point> 列表
      */
-    private static List<Point> getRandomPointList(double radio) {
+    private static List<Point> getRandomPointList(double radio, Integer blockNum) {
         ArrayList<Point> points = new ArrayList<>();
-        for (int i = 0; i < BLOCK_NUM / 2; i++) {
-            for (int j = 0; j < BLOCK_NUM; j++) {
+        for (int i = 0; i < blockNum / 2; i++) {
+            for (int j = 0; j < blockNum; j++) {
                 if (Math.random() < radio) {
                     points.add(new Point(i, j));
                 }
             }
         }
-        addReversePoints(points);
-        if (BLOCK_NUM % 2 == 1) {
-            for (int i = 0; i < BLOCK_NUM; i++) {
+        addReversePoints(points, blockNum);
+        if (blockNum % 2 == 1) {
+            for (int i = 0; i < blockNum; i++) {
                 if (Math.random() < radio) {
-                    points.add(new Point(BLOCK_NUM / 2, i));
+                    points.add(new Point(blockNum / 2, i));
                 }
             }
         }
@@ -342,10 +453,10 @@ public class RandomAvatarController {
      *
      * @param points point的列表
      */
-    private static void addReversePoints(List<Point> points) {
+    private static void addReversePoints(List<Point> points, Integer blockNum) {
         ArrayList<Point> pointListCopy = new ArrayList<>(points);
         for (Point point : pointListCopy) {
-            points.add(new Point((BLOCK_NUM - 1 - point.x), point.y));
+            points.add(new Point((blockNum - 1 - point.x), point.y));
         }
     }
 
