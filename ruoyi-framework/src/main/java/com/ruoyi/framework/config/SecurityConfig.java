129cd2ed4f3049b0ccf14f80163a241ce29dbaef
fx67ll <2717745014@qq.com>
2023-08-10 20:53:19 +0800

    forbidden lottery open api

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
@@ -111,11 +111,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/login", "/register", "/captchaImage").permitAll()
                 // 随机生成马赛克头像，可匿名访问
                 .antMatchers("/getRandomAvatar").permitAll()
-                // 为彩票管理开放部分api
-                .antMatchers("/lottery/chase/getLotteryChaseListForApp",
-                        "/lottery/setting/getLotterySettingConfigForApp/**",
-                        "/lottery/setting/editLotterySettingConfigForApp",
-                        "/lottery/log/addLotteryLogForApp").permitAll()
                 // 静态资源，可匿名访问
                 .antMatchers(HttpMethod.GET, "/", "/*.html", "/**/*.html", "/**/*.css", "/**/*.js", "/profile/**").permitAll()
                 .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/*/api-docs", "/druid/**").permitAll()
