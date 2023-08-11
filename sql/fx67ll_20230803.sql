7b2ba924bdef9000eda90780c169de59ae0951a1
fx67ll <2717745014@qq.com>
2023-08-11 09:20:09 +0800

    optimze

--- sql/fx67ll_20230803.sql
+++ sql/fx67ll_20230803.sql
@@ -10,7 +10,7 @@ create table fx67ll_lottery_log (
   is_win               char(1)         default 'N'                comment '是否中奖（Y是 N否）',
   winning_price        varchar(10)     default '0'                comment '中奖金额',
   number_type          int(1)                                     comment '当日购买的彩票类型（1大乐透 2双色球）',
-  week_type            int(1)                                     comment '星期几（1周一 2周二 3周三 4周四 5周五 6周六 7周日）',
+  week_type            int(1)                                     comment '星期几的购买记录（1周一 2周二 3周三 4周四 5周五 6周六 7周日）',
   has_more_purchases   char(1)         default 'N'                comment '是否有追加购买（Y是 N否）',
   del_flag             char(1)         default '0'                comment '删除标志（0代表存在 2代表删除）',
   user_id              bigint(20)                                 comment '用户ID',
@@ -48,9 +48,9 @@ create table fx67ll_lottery_chase (
 drop table if exists fx67ll_lottery_setting;
 create table fx67ll_lottery_setting (
   setting_id        bigint(20)      not null auto_increment    comment '生成配置主键',
-  user_id           bigint(20)                                 comment '用户ID',
   lottery_setting   varchar(1023)   default ''                 comment '个人彩票生成配置',
   del_flag          char(1)         default '0'                comment '删除标志（0代表存在 2代表删除）',
+  user_id           bigint(20)                                 comment '用户ID',
   create_by         varchar(64)     default ''                 comment '记录创建者',
   create_time 	    datetime                                   comment '记录创建时间',
   update_by         varchar(64)     default ''                 comment '记录更新者',
