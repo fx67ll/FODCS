ed7e58ea6d4e33ff2a0abd688816033ab04be6e0
fx67ll <2717745014@qq.com>
2023-12-13 17:36:28 +0800

    add sql for get total work time

--- sql/fx67ll_20230803.sql
+++ sql/fx67ll_20230803.sql
@@ -117,29 +117,29 @@ create table fx67ll_punch_log (
 -- ----------------------------
 -- 5-1、统计每个用户当月的工作总时长
 -- ----------------------------
-SELECT 
-    update_by AS user_id,
-    DATE_FORMAT(update_time, '%Y-%m') AS punch_month,
-    SUM(TIME_TO_SEC(TIMEDIFF(
-        (SELECT MAX(update_time) FROM fx67ll_punch_log WHERE DATE(update_time) = DATE(a.update_time) AND update_by = a.update_by),
-        (SELECT MIN(update_time) FROM fx67ll_punch_log WHERE DATE(update_time) = DATE(a.update_time) AND update_by = a.update_by)
-    ))) / 3600 AS total_work_hours,
-    SUM(TIMESTAMPDIFF(MINUTE, 
-        (SELECT MIN(update_time) FROM fx67ll_punch_log WHERE DATE(update_time) = DATE(a.update_time)), 
-        (SELECT MAX(update_time) FROM fx67ll_punch_log WHERE DATE(update_time) = DATE(a.update_time))
-    )) AS total_work_minutes,
-	SUM(TIME_TO_SEC(TIMEDIFF(
-        (SELECT MAX(update_time) FROM fx67ll_punch_log AS start_time 
-         WHERE DATE(start_time.update_time) = DATE(a.update_time) 
-         AND start_time.user_id = a.user_id),
-        (SELECT MIN(update_time) FROM fx67ll_punch_log AS end_time 
-         WHERE DATE(end_time.update_time) = DATE(a.update_time) 
-         AND end_time.user_id = a.user_id)
-    ))) AS total_work_seconds
-FROM 
-    fx67ll_punch_log a
-GROUP BY 
-    user_id, punch_month;
+SELECT
+	fx67ll_punch_log_result.punch_user,
+	fx67ll_punch_log_result.punch_month,
+	SUM(TIME_TO_SEC(TIMEDIFF( fx67ll_punch_log_result.max_punch_time, fx67ll_punch_log_result.min_punch_time ))) / 3600 AS total_work_hours,
+	SUM(TIMESTAMPDIFF(MINUTE, fx67ll_punch_log_result.min_punch_time, fx67ll_punch_log_result.max_punch_time )) AS total_work_minutes,
+	SUM(TIME_TO_SEC(TIMEDIFF( fx67ll_punch_log_result.max_punch_time, fx67ll_punch_log_result.min_punch_time ))) AS total_work_seconds
+FROM
+	(
+	SELECT
+		update_by AS punch_user,
+		DATE_FORMAT(update_time, '%Y-%m') AS punch_month,
+		DATE(update_time) AS punch_day,
+		MAX(update_time) AS max_punch_time,
+		MIN(update_time) AS min_punch_time
+	FROM
+		fx67ll_punch_log
+	GROUP BY
+		punch_user,
+		punch_month,
+		punch_day) 
+AS fx67ll_punch_log_result
+GROUP BY
+	punch_user;
 
     
    
