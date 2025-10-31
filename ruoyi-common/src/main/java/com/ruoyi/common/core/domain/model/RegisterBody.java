21d3c9323fd021d3783b42fa9dd1d41115ea01ae
“fx67ll;git config --global user.name “fx67ll <2717745014@qq.com>
2025-10-31 18:53:38 +0800

    add new register api for chaoshen

--- ruoyi-common/src/main/java/com/ruoyi/common/core/domain/model/RegisterBody.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/domain/model/RegisterBody.java
@@ -2,10 +2,20 @@ package com.ruoyi.common.core.domain.model;
 
 /**
  * 用户注册对象
- * 
+ *
  * @author ruoyi
  */
-public class RegisterBody extends LoginBody
-{
+public class RegisterBody extends LoginBody {
+    /**
+     * 备注信息
+     */
+    private String remark;
 
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
 }
